6f45384da8b631f2a0a91d3018b72fcc7c4cbc73
MonTonCode <montoncode@gmail.com>
2023-01-30 19:10:57 +0700

    เพิ่มหน้าจัดการการูปและแก้ไขการเชื่อมต่อ

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Router basename='/'>
       <App />
-    </React.StrictMode>
-  </Router>
+    </Router>
+  </React.StrictMode>
 )
